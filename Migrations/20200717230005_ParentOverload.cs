099f68f3df017a68c4e9029b13a914424933be6b
Julia Seidman <juliaseid@gmail.com>
2020-07-17 16:00:43 -0700

    overload parent constructor and add migration

---
+++ Migrations/20200717230005_ParentOverload.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ourVillage.Migrations
+{
+    public partial class ParentOverload : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Parent1ZipCode",
+                table: "Families");
+
+            migrationBuilder.RenameColumn(
+                name: "Parent1AddressLine2",
+                table: "Families",
+                newName: "Parent2Relationship");
+
+            migrationBuilder.RenameColumn(
+                name: "Parent1AddressLine1",
+                table: "Families",
+                newName: "Parent2Phone");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Parent2FirstName",
+                table: "Families",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Parent2LastName",
+                table: "Families",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Parent2FirstName",
+                table: "Families");
+
+            migrationBuilder.DropColumn(
+                name: "Parent2LastName",
+                table: "Families");
+
+            migrationBuilder.RenameColumn(
+                name: "Parent2Relationship",
+                table: "Families",
+                newName: "Parent1AddressLine2");
+
+            migrationBuilder.RenameColumn(
+                name: "Parent2Phone",
+                table: "Families",
+                newName: "Parent1AddressLine1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Parent1ZipCode",
+                table: "Families",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
--- Models/Family.cs
+++ Models/Family.cs
@@ -10,13 +10,31 @@ namespace ourVillage.Models
     public string Parent1LastName { get; set; }
     public string Parent1Phone { get; set; }
     public string Parent1Relationship { get; set; }
-    public string Parent1AddressLine1 { get; set; }
-    public string Parent1AddressLine2 { get; set; }
-    public int Parent1ZipCode { get; set; }
+    public string Parent2FirstName { get; set; }
+    public string Parent2LastName { get; set; }
+    public string Parent2Phone { get; set; }
+    public string Parent2Relationship { get; set; }
     public virtual ICollection<Contact> Contacts { get; set; }
     public virtual ICollection<Address> Addresses { get; set; }
     public virtual ICollection<Child> Children { get; set; }
+    public Family(string parent1FirstName, string parent1LastName, string parent1Phone, string parent1Relationship)
+    {
+      Parent1FirstName = parent1FirstName;
+      Parent1LastName = parent1LastName;
+      Parent1Phone = parent1Phone;
+      Parent1Relationship = parent1Relationship;
+    }
+    public Family(string parent1FirstName, string parent1LastName, string parent1Phone, string parent1Relationship, string parent2FirstName, string parent2LastName, string parent2Phone, string parent2Relationship)
+    {
+      Parent1FirstName = parent1FirstName;
+      Parent1LastName = parent1LastName;
+      Parent1Phone = parent1Phone;
+      Parent1Relationship = parent1Relationship;
+      Parent2FirstName = parent2FirstName;
+      Parent2LastName = parent2LastName;
+      Parent2Phone = parent2Phone;
+      Parent2Relationship = parent2Relationship;
+    }
   }
 }
 
-//this should be an overloaded constructor to allow up to 4 parents
