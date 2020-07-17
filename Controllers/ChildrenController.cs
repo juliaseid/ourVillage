abd1257aa907fa1473865fbfc47d64ace21d0502
Julia Seidman <juliaseid@gmail.com>
2020-07-17 14:10:32 -0700

    add children controller

---
+++ Controllers/ChildrenController.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Mvc;
+using ourVillage.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ourVillage.Controllers
+{
+  public class ChildrenController : Controller
+  {
+    private readonly ourVillageContext _db;
+
+    public ChildrenController(ourVillageContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Child> model = _db.Children.Include(children => children.Family).ToList();
+      model = model.OrderBy(c => c.Birthday).ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Child child)
+    {
+      _db.Children.Add(child);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Child thisChild = _db.Children.FirstOrDefault(children => children.ChildId == id);
+      return View(thisChild);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisChild = _db.Children.FirstOrDefault(children => children.ChildId == id);
+      return View(thisChild);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Child child)
+    {
+      _db.Entry(child).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisChild = _db.Children.FirstOrDefault(children => children.ChildId == id);
+      return View(thisChild);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisChild = _db.Children.FirstOrDefault(children => children.ChildId == id);
+      _db.Children.Remove(thisChild);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
---
+++ Controllers/FamilyController.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Mvc;
+using ourVillage.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace ourVillage.Controllers
+{
+  public class FamilyController : Controller
+  {
+    private readonly ourVillageContext _db;
+
+    public FamilyController(ourVillageContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Family> model = _db.Families.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Family family)
+    {
+      _db.Families.Add(family);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Family thisFamily = _db.Families.FirstOrDefault(family => family.FamilyId == id);
+      return View(thisFamily);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisFamily = _db.Families.FirstOrDefault(family => family.FamilyId == id);
+      return View(thisFamily);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Family family)
+    {
+      _db.Entry(family).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisFamily = _db.Families.FirstOrDefault(family => family.FamilyId == id);
+      return View(thisFamily);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisFamily = _db.Families.FirstOrDefault(family => family.FamilyId == id);
+      _db.Families.Remove(thisFamily);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
