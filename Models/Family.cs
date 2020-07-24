bf937028e66b175d2325cb0cb419f7d16e3d389f
Julia Seidman <juliaseid@gmail.com>
2020-07-24 12:24:48 -0700

    futz around with constructors and realize I didn't set up mySQL correctly

--- Models/Family.cs
+++ Models/Family.cs
@@ -4,6 +4,10 @@ namespace YourVillage.Models
 {
   public class Family
   {
+    // public Family()
+    // {
+
+    // }
     public int FamilyId { get; set; }
     public string ProfileName { get; set; }
     public string Parent1FirstName { get; set; }
@@ -17,24 +21,25 @@ namespace YourVillage.Models
     public virtual ICollection<Contact> Contacts { get; set; }
     public virtual ICollection<Address> Addresses { get; set; }
     public virtual ICollection<Child> Children { get; set; }
-    public Family(string parent1FirstName, string parent1LastName, string parent1Phone, string parent1Relationship)
-    {
-      Parent1FirstName = parent1FirstName;
-      Parent1LastName = parent1LastName;
-      Parent1Phone = parent1Phone;
-      Parent1Relationship = parent1Relationship;
-    }
-    public Family(string parent1FirstName, string parent1LastName, string parent1Phone, string parent1Relationship, string parent2FirstName, string parent2LastName, string parent2Phone, string parent2Relationship)
-    {
-      Parent1FirstName = parent1FirstName;
-      Parent1LastName = parent1LastName;
-      Parent1Phone = parent1Phone;
-      Parent1Relationship = parent1Relationship;
-      Parent2FirstName = parent2FirstName;
-      Parent2LastName = parent2LastName;
-      Parent2Phone = parent2Phone;
-      Parent2Relationship = parent2Relationship;
-    }
+
+    // public Family(string parent1FirstName, string parent1LastName, string parent1Phone, string parent1Relationship)
+    // {
+    //   Parent1FirstName = parent1FirstName;
+    //   Parent1LastName = parent1LastName;
+    //   Parent1Phone = parent1Phone;
+    //   Parent1Relationship = parent1Relationship;
+    // }
+    // public Family(string parent1FirstName, string parent1LastName, string parent1Phone, string parent1Relationship, string parent2FirstName, string parent2LastName, string parent2Phone, string parent2Relationship)
+    // {
+    //   Parent1FirstName = parent1FirstName;
+    //   Parent1LastName = parent1LastName;
+    //   Parent1Phone = parent1Phone;
+    //   Parent1Relationship = parent1Relationship;
+    //   Parent2FirstName = parent2FirstName;
+    //   Parent2LastName = parent2LastName;
+    //   Parent2Phone = parent2Phone;
+    //   Parent2Relationship = parent2Relationship;
+    // }
   }
 }
 
