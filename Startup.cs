95cf921558fc266dcb87317d302a52c12df1fcd9
Julia Seidman <juliaseid@gmail.com>
2020-07-28 10:01:10 -0700

    approach from c-sharpcorner but I don't think it will work

--- Startup.cs
+++ Startup.cs
@@ -29,9 +29,18 @@ namespace YourVillage
         .AddDbContext<YourVillageContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
 
-      services.AddIdentity<ApplicationUser, IdentityRole>()
+      services.AddDefaultIdentity<ApplicationUser>()
+      .AddRoles<IdentityRole>()
       .AddEntityFrameworkStores<YourVillageContext>()
       .AddDefaultTokenProviders();
+
+      //This is only available in .NetCore 3.0+
+      // services.AddAuthorization(options =>
+      // {
+      //   options.FallbackPolicy = new AuthorizationPolicyBuilder()
+      //   .RequireAuthenticatedUser()
+      //   .Build();
+      // });
 
       services.Configure<IdentityOptions>(options =>
         {
@@ -50,6 +59,7 @@ namespace YourVillage
       app.UseStaticFiles();
       app.UseDeveloperExceptionPage();
       app.UseAuthentication();
+
       app.UseMvc(routes =>
       {
         routes.MapRoute(
@@ -60,6 +70,63 @@ namespace YourVillage
       {
         await context.Response.WriteAsync("Something went wrong!");
       });
+
+      // ConfigureAuth(app);
+      createRolesandUsers();
     }
+
+    private void createRolesandUsers()
+    {
+
+      var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+      var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+      // In Startup iam creating first Admin Role and creating a default Admin User     
+      if (!roleManager.RoleExists("Admin"))
+      {
+
+        // first we create Admin rool    
+        var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+        role.Name = "Admin";
+        roleManager.Create(role);
+
+        //Here we create a Admin super user who will maintain the website                   
+
+        var user = new ApplicationUser();
+        user.UserName = "shanu";
+        user.Email = "syedshanumcain@gmail.com";
+
+        string userPWD = "A@Z200711";
+
+        var chkUser = UserManager.Create(user, userPWD);
+
+        //Add default User to Role Admin    
+        if (chkUser.Succeeded)
+        {
+          var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+        }
+      }
+
+      // creating Creating Manager role     
+      if (!roleManager.RoleExists("Manager"))
+      {
+        var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+        role.Name = "Manager";
+        roleManager.Create(role);
+
+      }
+
+      // creating Creating Employee role     
+      if (!roleManager.RoleExists("Employee"))
+      {
+        var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+        role.Name = "Employee";
+        roleManager.Create(role);
+
+      }
+    }
+
   }
 }
